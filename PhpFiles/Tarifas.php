11822d80890eb52ef31ec61f2205712e77f2d0d6
DESKTOP-A0E1H14\pablo <pablogomve@gmail.com>
2024-09-17 20:22:50 +0200

    Ruta de estilos mal escrita

--- PhpFiles/Tarifas.php
+++ PhpFiles/Tarifas.php
@@ -6,7 +6,7 @@ include_once("clases.php");
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width-scale=1">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="CSS/estilos.css">
     <title>&nbsp;</title>
 </head>
 
