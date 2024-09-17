e37ae67349bd5a44ee7602fb7dced25cffd8f5b0
DESKTOP-A0E1H14\pablo <pablogomve@gmail.com>
2024-09-17 19:36:40 +0200

    Añadir log cuando autoload clases

--- clases.php
+++ clases.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once("Clases/Logger.php");
 //Modificando la ruta de inclusión path
 $_ruta = dirname(__FILE__);
 $directories = array("Clases", "Clases/Mail", "Modulos");
@@ -16,6 +16,7 @@ if ($os == "WINNT") {
 
 foreach ($directories as $dir) {
     $path .= $separadores . $_ruta . $barras . $dir;
+    Logger::haz_log("GOVE", $path);
 }
 set_include_path($path);
 
